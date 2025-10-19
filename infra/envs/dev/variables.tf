ef8dbf6406295d48a0ff2266f8106c2f62fd8f4c
Jeremiah Coakley <fedlinllc@users.noreply.github.com>
2025-10-18 19:13:56 -0500

    fix(tf): expand envs/dev variable blocks to valid HCL

--- infra/envs/dev/variables.tf
+++ infra/envs/dev/variables.tf
@@ -1,3 +1,13 @@
-variable "project" { type = string }
-variable "env"     { type = string  default = "dev" }
-variable "region"  { type = string  default = "us-east-1" }
+variable "project" {
+  type = string
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-2"
+}
